9871fc3f216178f2a8d86ad0366588d83626cfc1
Shumskih <music.dll@mail.ru>
2017-10-01 11:23:35 +0300

    Java Core Final Project corrected

--- src/main/java/net/proselyte/Practice/javacorefinal/FinalTest.java
+++ src/main/java/net/proselyte/Practice/javacorefinal/FinalTest.java
@@ -1,70 +1,9 @@
 package main.java.net.proselyte.Practice.javacorefinal;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 public class FinalTest {
     public static void main(String[] args) {
 
         ConsoleHelper consoleHelper = new ConsoleHelper();
         consoleHelper.consoleHelper();
-
-
-//        DeveloperDAO developerDAO = new DeveloperDAO();
-//        SkillDAO skillDAO = new SkillDAO();
-//        ProjectDAO projectDAO = new ProjectDAO();
-//        CompanyDAO companyDAO = new CompanyDAO();
-//        CustomerDAO customerDAO = new CustomerDAO();
-//
-//        Set<Skill> skills = new LinkedHashSet<>();
-//        Set<Project> projects = new LinkedHashSet<>();
-//        Set<Developer> developers = new LinkedHashSet<>();
-
-
-//        Skill skill1 = new Skill(1, "Spring");
-//        Skill skill2 = new Skill(2, "Hibernate");
-//        Skill skill3 = new Skill(3, "HTML5");
-//        Skill skill4 = new Skill(4, "CSS");
-//
-//        Project project1 = new Project(1, "FileSystem", "1.2.2", developers);
-//        Project project2 = new Project(1, "Game", "2.10.7", developers);
-
-//        Company company = new Company(1, "Axis", projects);
-//
-//        Customer customer = new Customer(1, "Jan Kulikov", projects);
-
-//        Developer developer = new Developer(1, "Maks", "Ivanov", "Java Developer", skills, projects ,3,2000);
-//
-//        skills.add(skill1);
-//        skills.add(skill2);
-//        skills.add(skill3);
-//
-//        developers.add(developer);
-//        projects.add(project1);
-//        projectDAO.save(project1);
-//        projectDAO.update(project1);
-
-//        companyDAO.save(company);
-
-//        customerDAO.save(customer);
-//        companyDAO.update(company);
-//
-//
-//        developerDAO.save(developer);
-//        developerDAO.update(developer);
-
-//        System.out.println(developer.getId());
-
-//        skillDAO.save(skill1);
-//        skillDAO.save(skill2);
-//        skillDAO.save(skill3);
-//        skillDAO.save(skill4);
-
-        // getById
-//        projectDAO.getByIdDAO(1);
-//        developerDAO.getById(1);
-//        companyDAO.getByNameDAO("Axis");
-//        customerDAO.getByIdDAO(1);
-
     }
 }
